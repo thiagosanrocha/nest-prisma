7fd5d3a911034d273a37cff7f0a40013ba6f120f
Thiago Rocha <thiagorocha010@gmail.com>
2024-09-14 21:20:11 -0300

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ConflictInterceptor());
   app.useGlobalInterceptors(new DatabaseInterceptor());
